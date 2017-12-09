994c4d8dcbef7158b54ea547e103a1e12571bc27
Christopher Diep <diep.christopher@gmail.com>
2017-12-09 10:48:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ new_collection = []
+ counter = 0
+ while counter < collection.size
+   if collection[counter].even?
+     new_collection << collection[counter]
+   end
+   counter += 1
+ end
+ new_collection
 end
